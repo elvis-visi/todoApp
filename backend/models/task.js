2231dc883976a3dc806f570e9938a365979dd58c
elvis-visi <sivisevi1@gmail.com>
2024-04-06 16:55:30 +0200

    Added user model, it has references to the tasks model

--- backend/models/task.js
+++ backend/models/task.js
@@ -30,6 +30,18 @@ const taskSchema = new mongoose.Schema({
         type: Boolean,
         default: false
       },
+      user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+})
+
+taskSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Task = mongoose.model('Task', taskSchema)
