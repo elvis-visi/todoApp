b0f63890612bf203b7ddaa62eb454045663714ee
elvis-visi <sivisevi1@gmail.com>
2024-04-08 10:22:02 +0200

    Added test helper file, initialTask array and function to fetch all tasks

--- backend/tests/task_api.test.js
+++ backend/tests/task_api.test.js
@@ -4,26 +4,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert');
-
-const initialTasks = [
-    {
-        title: "Complete project proposal",
-        description: "Finalize the project proposal and submit it for review",
-        dueDate: "2023-06-15T00:00:00.000Z",
-        dateAdded: "2024-04-06T11:34:15.807Z",
-        priority: "high",
-        completed: false,
-    },
-    {
-        title: "2nd task",
-        description: "added a second task",
-        dueDate: "2023-06-15T00:10:00.000Z",
-        dateAdded: "2024-05-06T11:34:15.807Z",
-        priority: "medium",
-        completed: false,
-    },
-
-]
+const helper = require('./test_helper')
 
 //allows you to simulate HTTP requests, inspect responses, and assert expected behavior
 const api = supertest(app)
@@ -32,9 +13,9 @@ const api = supertest(app)
 //we ensure that the database is in the same state before every test is run
 beforeEach(async () => {
     await Task.deleteMany({})
-    let taskObject = new Task(initialTasks[0])
+    let taskObject = new Task(helper.initialTasks[0])
     await taskObject.save()
-    taskObject = new Task(initialTasks[1])
+    taskObject = new Task(helper.initialTasks[1])
     await taskObject.save()
 })
 
@@ -42,22 +23,22 @@ beforeEach(async () => {
 
 test('tasks are returned as json', async () => {
     await api
-      .get('/api/tasks')
+      .get('/api/tasks') //It returns a response object that represents the HTTP response received from the server.
       .expect(200)
       .expect('Content-Type', /application\/json/)
   })
 
 test('there are two tasks', async () => {
-    const response = await api.get('/api/tasks')
-    console.log('response body ', response.body)
-    assert.strictEqual(response.body.length, initialTasks.length)
+    const response = await helper.tasksInDb() //directly interacts with the database using the Task model and retrieves all the tasks from the database.
+    console.log('response tasks ', response)
+    assert.strictEqual(response.length, helper.initialTasks.length)
 })  
 
 test('the first task title is about project proposal', async () => {
     //get all tasks -> get method
-    const response = await api.get('/api/tasks')
-    //focus on the titles of the tasks
-    const titles = response.body.map(ele => ele.title)
+    const response = await helper.tasksInDb()
+    
+    const titles = response.map(ele => ele.title)
     //assert 
     assert(titles.includes('Complete project proposal'))
 })
@@ -83,11 +64,11 @@ test('a valid task can be added ', async () => {
     .expect('Content-Type', /application\/json/)
 
     //get all the tasks and assert the length of the new updated list
-    const response = await api.get('/api/tasks')
-    assert.strictEqual(response.body.length, initialTasks.length  +1)
+    const response = await helper.tasksInDb()
+    assert.strictEqual(response.length, helper.initialTasks.length+1)
 
       //assert that the new task exists in the list
-    const titles = response.body.map(e => e.title)
+    const titles = response.map(e => e.title)
     assert(titles.includes('Task 3'))  
 
 })
@@ -106,8 +87,8 @@ test('task without title is not added', async() => {
     .send(newTask)
     .expect(400)
 
-    const response = await api.get('/api/tasks')
-    assert.strictEqual(response.body.length, initialTasks.length)
+    const response = await helper.tasksInDb()
+    assert.strictEqual(response.length, helper.initialTasks.length)
 
 })
 
@@ -126,4 +107,11 @@ test('task without title is not added', async() => {
   By using the supertest library and the api instance, you can write tests that simulate HTTP requests 
   and assertions against the Express application without having to start a separate server process.
   supertest takes care that the application being tested is started at the port that it uses internally.
+
+
+
+  So, while await api.get('/api/tasks') sends an HTTP request to the server and returns a response object with headers, status, and body,
+   await helper.tasksInDb() directly interacts with the database and returns the task documents converted to plain JavaScript objects.
+ The first test case focuses on testing the API endpoint and the response it sends back, while the second test case focuses on checking
+ the actual data stored in the database and comparing it with the expected data defined in the initialTasks array.
   */
---
+++ backend/tests/test_helper.js
@@ -0,0 +1,34 @@
+// import the Task model
+const Task = require('../models/task')
+
+// initialTasks array
+const initialTasks = [
+    {
+        title: "Complete project proposal",
+        description: "Finalize the project proposal and submit it for review",
+        dueDate: "2023-06-15T00:00:00.000Z",
+        dateAdded: "2024-04-06T11:34:15.807Z",
+        priority: "high",
+        completed: false,
+    },
+    {
+        title: "2nd task",
+        description: "added a second task",
+        dueDate: "2023-06-15T00:10:00.000Z",
+        dateAdded: "2024-05-06T11:34:15.807Z",
+        priority: "medium",
+        completed: false,
+    },
+
+]
+
+
+//fetch all tasks
+const tasksInDb = async () => {
+    const tasks = await Task.find({})
+    return tasks.map(task => task.toJSON())
+}
+
+module.exports =  {
+    initialTasks, tasksInDb
+}
