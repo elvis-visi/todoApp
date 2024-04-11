ac2cb5e4f0894a4ecdb88150a34c354f042c87c6
elvis-visi <sivisevi1@gmail.com>
2024-04-11 12:03:07 +0200

    Tasks can be created only by authenticated users

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,6 +1,7 @@
 const taskRouter =require('express').Router()
 const Task = require('../models/task')
-
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 
 taskRouter.get('/',async (request, response) => {
@@ -33,8 +34,29 @@ taskRouter.delete('/:id', async (request, response, next) => {
 })
 
 
-taskRouter.post('/', async (request,response) => {
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization') // 'bearer token'
+
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
+taskRouter.post('/', async (request,response,next) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if(!decodedToken.id) {
+      return response.status(401).json({error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+      return response.status(404).json({ error: 'User not found' });
+    }
 
     if (!body.title) {
         return response.status(400).json({error: 'title is required'})
@@ -46,11 +68,15 @@ taskRouter.post('/', async (request,response) => {
         dueDate: body.dueDate,
         dateAdded: body.dateAdded,
         priority: body.priority,
-        completed: body.completed
+        completed: body.completed,
+        user: user._id
     })
 
     try{
         const savedTask = await task.save()
+        user.tasks = user.tasks.concat(savedTask._id)
+        await user.save()
+
         response.status(201).json(savedTask)
     }catch(exception){
         next(exception)
