c7b4d0e6cc261cc0708dc0f1a9d6303e94cb3009
elvis-visi <sivisevi1@gmail.com>
2024-04-26 11:29:37 +0200

    Styling Header

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -9,7 +9,6 @@ export default function Menu({searchTerm, setSearchTerm}) {
         />
         <ul className="menu-items">
           <li><a href="/stats">Stats</a></li>
-          <li><a href="/today">Today</a></li>
           <li><a href="/logout">Log Out</a></li>
         </ul>
       </div>
