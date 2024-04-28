97dbb4180823c1ac229cd06db7d7bbced62a0be2
elvis-visi <sivisevi1@gmail.com>
2024-04-28 14:46:09 +0200

    Styling reschedule button

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -7,9 +7,9 @@ export default function Menu({searchTerm, setSearchTerm, handleLogout}) {
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value)}
         />
-        <ul className="menu-items">
-          <li><button onClick={handleLogout}>Log Out</button></li>
-        </ul>
+        
+          <button onClick={handleLogout}>Log Out</button>
+        
       </div>
     );
   }
